2513eaa42a12cb7b55cfcac21a2a2b63fe2ec6e4
redi2213 <redi2213@users.noreply.github.com>
2026-08-11 01:00:48 +0330

    Clear branch for v1.4 refactor

--- screens/base.py
+++
@@ -1,9 +0,0 @@
-class BaseScreen:
-    def __init__(self, app):
-        self.app = app
-
-    def clear(self):
-        self.app.clear_content()
-
-    def add(self, widget):
-        self.app.add(widget)
